37f1890dc86a6a8f7505865b5091fcc2a7274d16
Sherman <hermangonzalezr@gmail.com>
2019-02-04 21:18:18 -0300

    Modificacion en alimentacion por BDD
    Se modifica la ventanaPokedex para que se alimente segun la cantidad de pokemones en BDD de manera automatica

--- src/controlador/ControladorPokedex.java
+++ src/controlador/ControladorPokedex.java
@@ -2,43 +2,63 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import modelo.BDD;
 import vista.VentanaPokedex;
 
 public final class ControladorPokedex extends controlador implements ActionListener {
 
-	VentanaPokedex vPokedex;
-	ControladorPrincipal cPrincipal;
-	private String pokemon;
+    VentanaPokedex vPokedex;
+    ControladorPrincipal cPrincipal;
+    private String pokemon;
+    BDD data;
 
-	public ControladorPokedex() {
-		vPokedex = new VentanaPokedex();
-		setListener();
+    public ControladorPokedex() {
+        data = new BDD();
+        vPokedex = new VentanaPokedex(cantidadFilas(), cantidadPokemones());
+        setListener();
+    }
 
-	}
+    void setListener() {
+        for (int i = 0; i < vPokedex.listaBotones.size(); i++) {
+            vPokedex.listaBotones.get(i).addActionListener(this);
+        }
+    }
 
-	void setListener() {
-		for (int i = 0; i < vPokedex.listaBotones.size(); i++) {
-			vPokedex.listaBotones.get(i).addActionListener(this);
-		}
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        pokemon = e.getActionCommand();
+        Notificar();
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		pokemon = e.getActionCommand();
-		Notificar();
-	}
+    @Override
+    public void Notificar() {
+        cPrincipal.MostrarPokemon();
+    }
 
-	@Override
-	public void Notificar() {
-		cPrincipal.MostrarPokemon();
-	}
+    public void SetcPrincipal(ControladorPrincipal cPrincipal) {
+        this.cPrincipal = cPrincipal;
+    }
 
-	public void SetcPrincipal(ControladorPrincipal cPrincipal) {
-		this.cPrincipal = cPrincipal;
-	}
+    public String getPokemon() {
+        return pokemon;
+    }
 
-	public String getPokemon() {
-		return pokemon;
-	}
-
+    public int cantidadFilas()
+    {
+      int cantidadTmp = data.cantidadPokemones();
+      int cantidadFilas;
+        if (cantidadTmp%5 == 0) {
+            cantidadFilas = (cantidadTmp/5);
+        }
+        else{
+            cantidadFilas = (cantidadTmp/5) + 1 ;
+        }
+        return cantidadFilas;
+    }
+    
+    public int cantidadPokemones()
+    {
+        int cantidadPokemones = data.cantidadPokemones();
+        return cantidadPokemones;
+    }
 }
