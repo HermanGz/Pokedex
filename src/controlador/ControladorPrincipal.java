a0dc8007d8779cf0765b580e5dc8293285e25b22
Sherman <hermangonzalezr@gmail.com>
2019-02-06 22:09:01 -0300

    Modificacion a controlador principal
    Se limpia un poco el codigo para que sea mas legible

--- src/controlador/ControladorPrincipal.java
+++ src/controlador/ControladorPrincipal.java
@@ -6,49 +6,49 @@ import vista.VentanaPokemon;
 
 public final class ControladorPrincipal {
 
-	ControladorLogin cLogin;
-	ControladorPokedex cPokedex;
-	VentanaLogin vLogin;
-	VentanaPokedex vPokedex;
-	VentanaPokemon vPokemon;
-	ControladorPokemon cPokemon;
+    ControladorLogin cLogin;
+    ControladorPokedex cPokedex;
+    ControladorPokemon cPokemon;
+    VentanaLogin vLogin;
+    VentanaPokedex vPokedex;
+    VentanaPokemon vPokemon;
 
-	public void IniciarComponentes() {
-		cLogin = new ControladorLogin();
-		cPokedex = new ControladorPokedex();
-		cPokemon = new ControladorPokemon();
+    public void IniciarComponentes() {
+        cLogin = new ControladorLogin();
+        cPokedex = new ControladorPokedex();
+        cPokemon = new ControladorPokemon();
 
-		cPokemon.SetcPrincipal(this);
-		cLogin.SetcPrincipal(this);
-		cPokedex.SetcPrincipal(this);
+        cPokemon.SetcPrincipal(this);
+        cLogin.SetcPrincipal(this);
+        cPokedex.SetcPrincipal(this);
 
-		vLogin = cLogin.vLogin;
-		vPokedex = cPokedex.vPokedex;
-		vPokemon = cPokemon.vPokemon;
-	}
+        vLogin = cLogin.vLogin;
+        vPokedex = cPokedex.vPokedex;
+        vPokemon = cPokemon.vPokemon;
+    }
 
-	public void ValidarLogin() {
-		if (cLogin.isLogSuccessfull()) {
-			vPokedex.setVisible(true);
-			vLogin.dispose();
-		}
-	}
+    public void ValidarLogin() {
+        if (cLogin.isLogSuccessfull()) {
+            vPokedex.setVisible(true);
+            vLogin.dispose();
+        }
+    }
 
-	public void MostrarPokemon() {
+    public void MostrarPokemon() {
 
-		String pokemonID = cPokedex.getPokemon();
+        String pokemonID = cPokedex.getPokemon();
 
-		cPokemon.setPokemon(pokemonID);
+        cPokemon.setPokemon(pokemonID);
 
-		vPokedex.setVisible(false);
-		vPokemon.setVisible(true);
-		System.out.println(pokemonID);
-	}
+        vPokedex.setVisible(false);
+        vPokemon.setVisible(true);
+        System.out.println(pokemonID);
+    }
 
-	public void VolverPokedex() {
+    public void VolverPokedex() {
 
-		vPokedex.setVisible(true);
-		vPokemon.setVisible(false);
-		vPokemon.dispose();
-	}
+        vPokedex.setVisible(true);
+        vPokemon.setVisible(false);
+        vPokemon.dispose();
+    }
 }
